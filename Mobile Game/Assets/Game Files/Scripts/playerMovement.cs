b9028def21d1b27ab2a7002e47216ca503b4c3c9
ABHMughal <ABHMughal@users.noreply.github.com>
2020-05-14 17:10:59 +0500

    Project Added

---
+++ Mobile Game/Assets/Game Files/Scripts/playerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speedPower;
+    public float jumpPower;
+    
+    private bool ground;
+    private int action;
+    private Rigidbody2D rigidBody;
+    private Animator animator;
+    private SpriteRenderer sprRenderer;
+
+    void Start()
+    {
+        ground = false;
+        action = 0;
+        rigidBody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputX = Input.GetAxis("Horizontal");
+        rigidBody.velocity = new Vector2(inputX * speedPower, rigidBody.velocity.y);
+
+        if (inputX > 0)
+            sprRenderer.flipX = true;
+        else if (inputX < 0)
+            sprRenderer.flipX = false;
+        
+
+        if (Input.GetKeyDown("space") && ground)
+        {
+            ground = false;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, jumpPower);
+
+        }
+        
+        if (ground && (inputX > 0 || inputX < 0))
+        {
+            
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.collider.CompareTag("Ground"))
+        {
+            ground = true;
+        }
+    }
+}
